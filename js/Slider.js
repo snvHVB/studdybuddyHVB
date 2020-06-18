2be68daeae152ea58ea8f30e42244da3875c9bd9
ASUSFIERENS <dries.fierens@student.odisee.be>
2020-06-18 19:58:28 +0200

    changed slider

--- js/Slider.js
+++ js/Slider.js
@@ -5,5 +5,18 @@ const navSlide = () => {
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
     });
+    let status = false;
+    window.onclick = function(event){
+        const nav = document.querySelector('.nav-links');
+
+        if (event.target == box){
+            nav.classList.toggle('nav-active');
+            status = true;
+            console.log(status);
+        }else if (event.target != menu && status == true){
+            nav.classList.toggle('nav-active');
+            status = false;
+        }
+    }
 }
 navSlide();

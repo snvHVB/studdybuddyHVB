d89be1465ab2c848fc5caf3f899bc8ff0c7bd8fc
Bladgroen <67104310+Bladgroen@users.noreply.github.com>
2020-06-18 18:19:32 +0200

    update voor het menu

--- js/Slider.js
+++ js/Slider.js
@@ -1,9 +1,17 @@
-const navSlide = () => {
-    const burger = document.querySelector('.box');
+let menu = document.getElementById('menu');
+let box = document.getElementById('hmbrg');
+let status = false;
+window.onclick = function(event){
     const nav = document.querySelector('.nav-links');
 
-    burger.addEventListener('click', () => {
+    if (event.target == box){
         nav.classList.toggle('nav-active');
-    });
+        status = true;
+        console.log(status);
+    }else if (event.target != menu && status == true){
+        nav.classList.toggle('nav-active');
+        status = false;
+    }
+
+
 }
-navSlide();
--- js/maakKader.js
+++ js/maakKader.js
@@ -101,3 +101,5 @@ function opvullen(velden){
     }
 }
 
+
+
